cf4dbabad4582ed599bcb01f38a2948145a7b8ea
Corey Burns <coreymburns@gmail.com>
2021-02-13 17:46:21 -0500

    added basic structure for each product type shop page

---
+++ src/pages/shop/chicken/index.tsx
@@ -0,0 +1,62 @@
+import Container from "@components/container"
+import firebaseAdmin from "@config/firebaseAdmin"
+import firebaseClient from "@config/firebaseClient"
+import "firebase/firestore"
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next"
+import nookies from "nookies"
+
+export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
+  try {
+    const db = firebaseAdmin.firestore()
+
+    const cookies = nookies.get(ctx)
+    const token = await firebaseAdmin.auth().verifyIdToken(cookies.token)
+    const { uid, email } = token
+
+    const collections = db.collection("collections")
+    const chickenDoc = await collections.doc("Chicken").get()
+
+    return {
+      props: {
+        data: chickenDoc.data(),
+        message: `Your email is ${email} and your UID is ${uid}.`
+      }
+    }
+  } catch (err) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: "/login"
+      },
+      // `as never` is required for correct type inference
+      // by InferGetServerSidePropsType below
+      props: {} as never
+    }
+  }
+}
+
+const AuthenticatedPage = (
+  props: InferGetServerSidePropsType<typeof getServerSideProps>
+) => (
+  <Container>
+    <p>{props.message!}</p>
+    <h1>{props?.data?.title}</h1>
+    <h2>items : </h2>
+    <ul>
+      {props?.data?.items.map((item: any) => (
+        <li key={item.name}>{item.name}</li>
+      ))}
+    </ul>
+    <button
+      type="submit"
+      onClick={async () => {
+        await firebaseClient.auth().signOut()
+        window.location.href = "/"
+      }}
+    >
+      Sign out
+    </button>
+  </Container>
+)
+
+export default AuthenticatedPage
