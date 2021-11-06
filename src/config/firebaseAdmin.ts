fc0222bc7920f52bf25e1fa1c62ead2d2d0d6b7d
Corey <coreymburns@gmail.com>
2021-11-06 08:48:10 -0400

    importing firebase service account directly...see if it works

--- src/config/firebaseAdmin.ts
+++ src/config/firebaseAdmin.ts
@@ -1,8 +1,9 @@
 import * as firebaseAdmin from "firebase-admin"
+import firebaseJSON from "../../quikeats-d24d2-5af7b1f0820a.json"
 
-const privateKey = process.env.FIREBASE_PRIVATE_KEY
-const clientEmail = process.env.FIREBASE_CLIENT_EMAIL
-const projectId = process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID
+const privateKey = firebaseJSON.private_key
+const clientEmail = firebaseJSON.client_email
+const projectId = firebaseJSON.project_id
 
 if (!firebaseAdmin.apps.length) {
   firebaseAdmin.initializeApp({
