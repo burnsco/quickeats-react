e4cbca131041e1f9752e7ded759c049c75acdc33
Corey Burns <coreymburns@gmail.com>
2022-03-09 15:56:25 -0500

    updated deps, fixed cards

---
+++ src/components/common/ProductCard.tsx
@@ -0,0 +1,96 @@
+import { AddIcon } from "@chakra-ui/icons"
+import {
+  Badge,
+  Box,
+  Button,
+  HStack,
+  useColorModeValue,
+  useToast
+} from "@chakra-ui/react"
+import { useCart } from "@hooks/cart/cart"
+import Image from "next/image"
+
+export default function ProductCard({ item, routeName }: any) {
+  console.log("productCard", item, routeName)
+  const borderColor = useColorModeValue("gray.100", "#313131")
+  const hoverColor = useColorModeValue("gray.200", "gray.600")
+  const bg = useColorModeValue("whitesmoke", "#202020")
+  const { dispatch } = useCart()
+
+  const toast = useToast()
+
+  const handleAddItem = (item: CartItem) =>
+    dispatch({
+      type: "ADD_ITEM",
+      payload: {
+        id: item.id,
+        name: item.name,
+        price: item.price,
+        routeName: item.routeName,
+        qty: 1
+      }
+    })
+
+  return (
+    <Box
+      bg={bg}
+      borderColor={borderColor}
+      key={item.id}
+      maxW="md"
+      borderWidth="1px"
+      borderRadius="md"
+      shadow="md"
+      _hover={{
+        boxShadow: "md",
+        borderWidth: "1px",
+        borderColor: hoverColor
+      }}
+      overflow="hidden"
+    >
+      <Box pos="relative" maxW="md" h="260px">
+        <Image
+          layout="fill"
+          objectFit="cover"
+          src={`/${routeName}/${item.id}`}
+          alt={`image-${item.title}`}
+        />
+      </Box>
+      <Box p={4}>
+        <HStack>
+          <Badge borderRadius="full" px="2" colorScheme="orange">
+            ${item.price}
+          </Badge>
+          <Box
+            ml={2}
+            fontWeight="semibold"
+            fontSize="md"
+            as="h3"
+            lineHeight="tight"
+            isTruncated
+          >
+            {item.name}
+          </Box>
+        </HStack>
+        <Button
+          size="sm"
+          w="full"
+          colorScheme="purple"
+          mt={4}
+          leftIcon={<AddIcon />}
+          mr="-px"
+          onClick={() => {
+            handleAddItem(item)
+            toast({
+              status: "info",
+              title: `${item.name} was added to your cart.`,
+              variant: "top-accent",
+              isClosable: true
+            })
+          }}
+        >
+          Add To Cart
+        </Button>
+      </Box>
+    </Box>
+  )
+}
--- src/components/common/ProductsList.tsx
+++ src/components/common/ProductsList.tsx
@@ -1,37 +1,13 @@
-import { AddIcon } from "@chakra-ui/icons"
-import {
-  Badge,
-  Box,
-  Button,
-  Container,
-  HStack,
-  SimpleGrid,
-  useColorModeValue,
-  useToast
-} from "@chakra-ui/react"
-import { useCart } from "@hooks/cart/cart"
+import { Container, SimpleGrid } from "@chakra-ui/react"
+import ProductAddToCart from "@components/common/ProductAddToCart"
 import "firebase/firestore"
-import Image from "next/image"
 
 export default function ProductsList(props: any) {
-  const borderColor = useColorModeValue("gray.100", "#313131")
-  const hoverColor = useColorModeValue("gray.200", "gray.600")
-  const bg = useColorModeValue("whitesmoke", "#202020")
-  const { dispatch } = useCart()
-
-  const toast = useToast()
-
-  const handleAddItem = (item: CartItem) =>
-    dispatch({
-      type: "ADD_ITEM",
-      payload: {
-        id: item.id,
-        name: item.name,
-        price: item.price,
-        routeName: item.routeName,
-        qty: 1
-      }
-    })
+  const { data } = props
+  const { items } = data
+  const { routeName } = props.data
+  console.log("productslist")
+  console.log(props)
 
   return (
     <Container maxW="xxl" mt="3rem">
@@ -41,69 +17,16 @@ export default function ProductsList(props: any) {
         spacing={4}
         w="full"
       >
-        {props?.data?.items.map((item: any) => (
-          <Box
-            bg={bg}
-            borderColor={borderColor}
-            key={item.id}
-            maxW="md"
-            borderWidth="1px"
-            borderRadius="md"
-            shadow="md"
-            _hover={{
-              boxShadow: "md",
-              borderWidth: "1px",
-              borderColor: hoverColor
-            }}
-            overflow="hidden"
-          >
-            <Box pos="relative" maxW="md" h="260px">
-              <Image
-                layout="fill"
-                objectFit="cover"
-                src={`/${props?.data?.routeName}/${item.id}`}
-                alt={`image-${item.title}`}
-              />
-            </Box>
-            <Box p={4}>
-              <HStack>
-                <Badge borderRadius="full" px="2" colorScheme="orange">
-                  ${item.price}
-                </Badge>
-                <Box
-                  ml={2}
-                  fontWeight="semibold"
-                  fontSize="md"
-                  as="h3"
-                  lineHeight="tight"
-                  isTruncated
-                >
-                  {item.name}
-                </Box>
-              </HStack>
-              <Button
-                size="sm"
-                spacing={6}
-                w="full"
-                colorScheme="purple"
-                mt={4}
-                leftIcon={<AddIcon />}
-                mr="-px"
-                onClick={() => {
-                  handleAddItem(item)
-                  toast({
-                    status: "info",
-                    title: `${item.name} was added to your cart.`,
-                    variant: "top-accent",
-                    isClosable: true
-                  })
-                }}
-              >
-                Add To Cart
-              </Button>
-            </Box>
-          </Box>
-        ))}
+        {items.map((item: any) => {
+          return (
+            // <ProductCard
+            //   key={`product-${item}`}
+            //   item={item}
+            //   routeName={routeName}
+            // />
+            <ProductAddToCart key={item.id} />
+          )
+        })}
       </SimpleGrid>
     </Container>
   )
